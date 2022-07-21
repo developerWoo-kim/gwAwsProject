e381dfb6fdd56105e8b2abef92f362237fa533d4
SEC <gunwo151@gmail.com>
2022-07-21 17:58:35 +0900

    웹훅 테스트2

--- src/main/java/gwAws/gwAwsProject/IndexController.java
+++ src/main/java/gwAws/gwAwsProject/IndexController.java
@@ -5,15 +5,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
-
+    /**
+     * 웹훅 테스트
+     * @return
+     */
     @RequestMapping("/home")
     public String home() {
         return "home";
-
-
-
-
-
-
     }
 }
